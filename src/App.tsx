165173df1d340fe4873c03b70e838a5461348897
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-05-06 18:06:53 -0300

    feat: Adição do input de tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import styles from './App.module.css';
 import { Header } from './components/Header';
-
+import { PlusCircle } from '@phosphor-icons/react';
 import './global.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button type="submit">
+          Criar
+          <PlusCircle size={20} />
+        </button>
+      </div>
     </>
   )
 }
