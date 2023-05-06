0d0a945e3329fe4a69b7520964c44368c012afaa
Rafaela Miranda <rafaela-gomes@outlook.com>
2023-05-06 17:45:30 -0300

    feat: Criação do Header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+import { Header } from './components/Header';
+
+import './global.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
